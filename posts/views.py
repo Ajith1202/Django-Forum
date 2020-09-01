8323fb61e96cb49dcacb354b7efad8d5e0e5ceaa
Ajith <ajith1202@gmail.com>
2020-09-01 18:04:06 +0530

    Added some error handling

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from .models import *
 from .serializers import *
 
+
 @api_view(['GET', 'POST'])
 def ListAPIView(request):
     if request.method == 'GET':
@@ -20,6 +21,7 @@ def ListAPIView(request):
         post = Post.objects.create(title=title, description=description, author=request.user)
         serializer = PostSerializer(post, many=False)
         return Response(serializer.data)  
+
 
 @api_view(['GET', 'PUT'])
 def DetailAPIView(request, pk):
@@ -51,24 +53,31 @@ def DetailAPIView(request, pk):
     return Response(serialized_data)
 
 
-
 @api_view(['POST'])
 def CommentAddAPIView(request, pk):
-    post = Post.objects.get(id=pk)
+    try:
+        post = Post.objects.get(id=pk)
+    except DoesNotExist:
+        return Response("The given Question does not exist...")
     comment = Comment.objects.create(description=request.data.get('description'), author=request.user, post=post)
     serialized_data = CommentSerializer(comment, many=False)
     if request.data.get("description"):
         return Response(serialized_data.data)    
 
+
 @api_view(['GET'])
 def PostVoteAddAPIView(request, post_id, vote_type):
-    post = Post.objects.get(id=post_id)
+    try:
+        post = Post.objects.get(id=post_id)
+    except DoesNotExist:
+        return Response("The given Question does not exist...")
     if vote_type == 1:
         vote = PostVote.objects.create(post=post, author=request.user, vote_type=1)
     elif vote_type == 0:
         vote = PostVote.objects.create(post=post, author=request.user, vote_type=-1)
 
     return Response(PostVoteSerializer(vote, many=False).data)
+
 
 @api_view(['DELETE'])
 def PostDeleteAPIView(request, pk):
