a539966e393d0847d25882b465386500965f69df
Ajith <ajith1202@gmail.com>
2020-09-02 21:37:58 +0530

    Added comments to views.py

--- posts/views.py
+++ posts/views.py
@@ -4,43 +4,46 @@ from .models import *
 from .serializers import *
 
 
+# LIST ALL THE QUESTIONS , ADD A QUESTION
 @api_view(['GET', 'POST'])
 def ListAPIView(request):
-    if request.method == 'GET':
+    if request.method == 'GET':    # GET REQUEST
         queryset = Post.objects.all()
         serializer = PostSerializer(queryset, many=True)
         data = serializer.data
         for post in data:
-            item = Post.objects.get(id=post["id"])
+            item = Post.objects.get(id=post["id"])    # FOR ACCESSING EACH POST
             post["Upvotes"] = item.upvotes()
             post["Downvotes"] = item.downvotes()
         return Response(data)
-    elif request.method == 'POST':
+    elif request.method == 'POST':    # POST REQUEST
         title = request.data.get('title')
         description = request.data.get('description')
-        post = Post.objects.create(title=title, description=description, author=request.user)
+        post = Post.objects.create(title=title, description=description, author=request.user)    # THE CURRENT USER WILL BE THE AUTHOR.
         serializer = PostSerializer(post, many=False)
         return Response(serializer.data)  
 
 
+# DETAILED VIEW OF A QUESTION , UPDATE A QUESTION
 @api_view(['GET', 'PUT'])
 def DetailAPIView(request, pk):
+    # TRY-EXCEPT CLAUSE IN-CASE THE QUESTION DOES NOT EXIST
     try:
         post = Post.objects.get(id=pk)
     except Post.DoesNotExist:
         return Response("The given Question does not exist...")
     serialized_data = PostSerializer(post, many=False).data
-    comments = post.comment_set.all()
+    comments = post.comment_set.all()    # FETCHING ALL THE COMMENTS OF THE GIVEN POST
     serialized_data["Comments"] = CommentSerializer(comments, many=True).data
     
-    if request.method == 'POST':
+    if request.method == 'POST':    # POST REQUEST
         serialized_data["Votes"] = {
             "Upvotes": post.upvotes(),
             "Downvotes": post.downvotes()
         }
         return Response(serialized_data)
-    elif request.method == 'PUT':
-        if post.author == request.user:
+    elif request.method == 'PUT':    # PUT REQUEST
+        if post.author == request.user:    # ONLY ALLOW UPDATION BY THE AUTHOR OF THE QUESTION
             serialized_data = PostSerializer(post, data=request.data, many=False)
             if serialized_data.is_valid():
                 serialized_data.save()
@@ -51,13 +54,14 @@ def DetailAPIView(request, pk):
             }
             serialized_data["Comments"] = CommentSerializer(comments, many=True).data
             return Response(serialized_data)
-        return Response("Sorry, you are not authorised to make changes to this Question...")    
+        return Response("Sorry, you are not authorised to make changes to this Question...")    # IF ANY OTHER USER OTHER THAN THE AUTHOR TRIES TO UPDATE A QUESTION    
 
     return Response(serialized_data)
 
 
 @api_view(['GET', 'POST'])
 def CommentAddAPIView(request, pk):
+    # TRY-EXCEPT CLAUSE IN-CASE THE QUESTION DOES NOT EXIST
     try:
         post = Post.objects.get(id=pk)
     except Post.DoesNotExist:
@@ -66,17 +70,19 @@ def CommentAddAPIView(request, pk):
     serialized_data = CommentSerializer(comment, many=False)
     if request.data.get("description"):
         return Response(serialized_data.data)    
+    return Response("Add content for your comment!!!")    # IF NO DESCRIPTION IS GIVEN FOR THE COMMENT
 
 
 @api_view(['GET'])
 def PostVoteAddAPIView(request, post_id, vote_type):
+    # TRY-EXCEPT CLAUSE IN-CASE THE QUESTION DOES NOT EXIST
     try:
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
         return Response("The given Question does not exist...")
-    if vote_type == 1:
+    if vote_type == 1:    # UPVOTE 
         vote = PostVote.objects.create(post=post, author=request.user, vote_type=1)
-    elif vote_type == 0:
+    elif vote_type == 0:    # DOWNVOTE
         vote = PostVote.objects.create(post=post, author=request.user, vote_type=-1)
 
     return Response(PostVoteSerializer(vote, many=False).data)
@@ -84,12 +90,13 @@ def PostVoteAddAPIView(request, post_id, vote_type):
 
 @api_view(['DELETE'])
 def PostDeleteAPIView(request, pk):
+    # TRY-EXCEPT CLAUSE IN-CASE THE QUESTION DOES NOT EXIST
     try:
         post = Post.objects.get(id=pk)
     except Post.DoesNotExist:
         return Response("The given Question does not exist...")
-    if post.author == request.user:
-        post.delete()    
+    if post.author == request.user:    # ONLY THE AUTHOR IS ALLOWED TO DELETE HIS/HER QUESTION
+        post.delete()
         return Response("Your Question has been successfully deleted...")
-    return Response("Sorry, you are not authorized to make changes to this question...")    
+    return Response("Sorry, you are not authorized to make changes to this question...")    # IF ANY OTHER USER ATTEMPTS TO DELETE A QUESTION
 
