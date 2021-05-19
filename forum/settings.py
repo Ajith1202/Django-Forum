86a1cab26cf87a2c87938cce7c1bbc2cdbd2f403
Ajith <ajith1202@gmail.com>
2021-05-19 17:04:34 +0530

    Added User registration using allauth

--- forum/settings.py
+++ forum/settings.py
@@ -29,6 +29,10 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
 
     'posts.apps.PostsConfig',
 ]
@@ -112,3 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
--- forum/urls.py
+++ forum/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('posts.urls')),
-    path('auth/', include('rest_auth.urls'))
+    path('auth/', include('rest_auth.urls')),
+    path('auth/registration/', include('rest_auth.registration.urls')),
 ]
