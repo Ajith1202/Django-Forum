568c63df47405c9dc873e635c359809a8dfda07a
Ajith <ajith1202@gmail.com>
2021-06-23 14:29:22 +0530

    Added comments to posts/models.py

--- posts/models.py
+++ posts/models.py
@@ -10,12 +10,12 @@ class Post(models.Model):
     view_count = models.IntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
-        return (str(self.title) + " by " + str(self.author.username))
+        return (str(self.title) + " by " + str(self.author.username))       # FOR THE ADMIN PANEL
 
-    def increment_view(self):
+    def increment_view(self):       # ANYTIME A POST IS VIEWED, THIS METHOD GETS CALLED.
         self.view_count = self.view_count + 1
 
-    def votes(self):
+    def votes(self):        # RETURNS THE NUMBER OF UPVOTES AND DOWNVOTES OF A POST, AS A TUPLE
         queryset = self.post_vote.all()
         a, b = 0, 0
         for i in queryset:
@@ -24,19 +24,19 @@ class Post(models.Model):
             if i.vote_type == -1:
                 b += 1
         return (a, b)
-    
-    def vote_score(self):
+
+    def vote_score(self):       # RETURNS (UPVOTES - DOWNVOTES) OF A POST
         queryset = self.post_vote.all()
         queryset = [i.vote_type for i in queryset]
         return sum(queryset)
-    
+
 
 class Comment(models.Model):
     description = models.TextField(null=False, blank=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=False, blank=False)
-    
-    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE) # PARENT FIELD, FOR REPLIES
 
     submitted_on = models.DateTimeField(auto_now_add=True)
 
@@ -45,10 +45,10 @@ class Comment(models.Model):
         return self.description
 
     def children(self):
-        return Comment.objects.filter(parent=self)
+        return Comment.objects.filter(parent=self)      # RETURNS THE CHILDREN OF THE COMMENT
 
     @property
-    def is_parent(self):
+    def is_parent(self):    # RETURNS IF THE COMMENT IS A TOP-LEVEL COMMENT OR NOT
         if self.parent is None:
             return True
         return False
